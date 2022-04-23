6abb777a1bb4ccdc19eb06a65b5c3a3feb4f58af
eivanova <ivanova.e@spb-ifuture.com>
2022-04-23 09:58:15 +0400

    add modals, fetch API

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -1,62 +1,127 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import PropTypes from "prop-types";
 import IngredientSection from "../ingredient-section/ingredient-section.js";
+import IngredientItem from "../ingredient-item/ingredient-item.js";
+import IngredientDetails from "../ingredient-details/ingredient-details.js";
+import Modal from "../modal/modal.js";
 import styles from "./burger-ingredients.module.scss";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 
-function BurgerIngredients({ cart, data }) {
+function BurgerIngredients({ items, cart }) {
+  const [displayModal, setDisplayModal] = useState(false);
+  const [clickedIngredienId, setClickedIngredienId] = useState(0);
+
+  const openModal = (e) => {
+    setDisplayModal(true);
+    setClickedIngredienId(e.currentTarget.id);
+  };
+
+  const closeModal = () => {
+    setDisplayModal(false);
+  };
+
+  const bunRef = useRef(null);
+  const sauceRef = useRef(null);
+  const mainRef = useRef(null);
+
+  const executeScroll = (ref) => (e) => {
+    if (ref && ref.current) {
+      ref.current.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    setCurrent();
+  };
+
   const [current, setCurrent] = React.useState("bun");
   return (
     <>
       <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
       <div className={`${styles.tabs}`}>
-        <Tab value="bun" active={current === "bun"} onClick={setCurrent}>
+        <Tab
+          value="bun"
+          active={current === "bun"}
+          onClick={executeScroll(bunRef)}
+        >
           Булки
         </Tab>
-        <Tab value="sauce" active={current === "sauce"} onClick={setCurrent}>
+        <Tab
+          value="sauce"
+          active={current === "sauce"}
+          onClick={executeScroll(sauceRef)}
+        >
           Соусы
         </Tab>
-        <Tab value="main" active={current === "main"} onClick={setCurrent}>
+        <Tab
+          value="main"
+          active={current === "main"}
+          onClick={executeScroll(mainRef)}
+        >
           Начинки
         </Tab>
       </div>
 
       <div className={`${styles.column_inner} mt-10`}>
-        <div id="bun" className="tabcontent">
-          <IngredientSection
-            key="bun"
-            sectionTitle="Булки"
-            type="bun"
-            cart={cart}
-            data={data}
-          />
+        <div id="bun" className="tabcontent" ref={bunRef}>
+          <IngredientSection sectionTitle="Булки">
+            {items
+              .filter((item) => item.type === "bun")
+              .map((item, index) => (
+                <IngredientItem
+                  item={item}
+                  cart={cart}
+                  key={item._id}
+                  customClickEvent={openModal}
+                />
+              ))}
+          </IngredientSection>
         </div>
-        <div id="sauce" className="tabcontent">
-          <IngredientSection
-            key="sauce"
-            sectionTitle="Соусы"
-            type="sauce"
-            cart={cart}
-            data={data}
-          />
+        <div id="sauce" className="tabcontent" ref={sauceRef}>
+          <IngredientSection sectionTitle="Соусы">
+            {items
+              .filter((item) => item.type === "sauce")
+              .map((item, index) => (
+                <IngredientItem
+                  item={item}
+                  cart={cart}
+                  key={item._id}
+                  customClickEvent={openModal}
+                />
+              ))}
+          </IngredientSection>
         </div>
-        <div id="main" className="tabcontent">
-          <IngredientSection
-            key="main"
-            sectionTitle="Начинки"
-            type="main"
-            cart={cart}
-            data={data}
-          />
+        <div id="main" className="tabcontent" ref={mainRef}>
+          <IngredientSection sectionTitle="Начинки">
+            {items
+              .filter((item) => item.type === "main")
+              .map((item, index) => (
+                <IngredientItem
+                  item={item}
+                  cart={cart}
+                  key={item._id}
+                  customClickEvent={openModal}
+                />
+              ))}
+          </IngredientSection>
         </div>
       </div>
+      <Modal
+        isOpen={displayModal}
+        closeMe={closeModal}
+        title={"Детали ингредиента"}
+      >
+        <IngredientDetails
+          item={items.find((item) => item._id === clickedIngredienId)}
+        />
+      </Modal>
     </>
   );
 }
 
 BurgerIngredients.propTypes = {
-  cart: PropTypes.arrayOf(PropTypes.object).isRequired,
-  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  cart: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default BurgerIngredients;
--- src/components/price-block/price-block.js
+++ src/components/price-block/price-block.js
@@ -1,26 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {
-  Button,
-  CurrencyIcon,
-} from "@ya.praktikum/react-developer-burger-ui-components";
+import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./price-block.module.scss";
 
-function PriceBlock({ total }) {
+function PriceBlock({ total, children }) {
   return (
     <section className={`${styles.price_block}`}>
       <div className={`${styles.price} text text_type_digits-medium mr-10`}>
         {total} <CurrencyIcon type="primary" />
       </div>
-      <Button type="primary" size="large">
-        Оформить заказ
-      </Button>
+      {children}
     </section>
   );
 }
 
 PriceBlock.propTypes = {
   total: PropTypes.number.isRequired,
+  children: PropTypes.object.isRequired,
 };
 
 export default PriceBlock;
